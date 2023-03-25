0fd8c8638ca7d5434ef6604435976d4192d334d8
arunar1 <arunarofficial00@gmail.com>
2023-03-25 15:35:20 +0530

    secondcommit

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express=require('express')
 
 const server=express()
 
+server.use(express.json());
+
+server.get('/',(req,res)=>{
+    res.send("Hello welcome to the page");
+});
+
 server.listen(4001,()=>{
     console.log("server is running successfully on port number 400");
 });
